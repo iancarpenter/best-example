7f604fd6a43429401b5eeba6730e0302782f45ff
Ian Carpenter <ianhcarpenter@gmail.com>
2019-07-30 19:12:00 +0100

    should have committed thsi as well!

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener(
     "DOMContentLoaded",
     function(event) {
-        alert('This page is loaded and the script ran!');
+        let message = document.getElementById('message');
+        message.innerText = 'The script ran!';
     }
 );
